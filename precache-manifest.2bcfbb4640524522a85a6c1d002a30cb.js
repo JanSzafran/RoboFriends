f4f58720c10617845777b818440bcad177fae520
JanSzaf <j.szafran.praca@gmail.com>
2019-03-19 08:51:16 +0100

    Updates

---
+++ precache-manifest.2bcfbb4640524522a85a6c1d002a30cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "af678f7a159ad81e3475",
+    "url": "/RoboFriends/static/js/main.007d8197.chunk.js"
+  },
+  {
+    "revision": "e67111e2209fdc653755",
+    "url": "/RoboFriends/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "af678f7a159ad81e3475",
+    "url": "/RoboFriends/static/css/main.daf64abc.chunk.css"
+  },
+  {
+    "revision": "e67111e2209fdc653755",
+    "url": "/RoboFriends/static/css/2.3cabffd0.chunk.css"
+  },
+  {
+    "revision": "1c5e989507ecbd5436a32ea5b394fccf",
+    "url": "/RoboFriends/index.html"
+  }
+];
